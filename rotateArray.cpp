d5208a4e7cf76f88063f546ec4d824e180a65645
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-26 20:11:57 +0530

    added problem

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -1,26 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void rotateArray(vector<int>arr,int size,int k){
-    cout<<"Calling function"<<endl;
-    vector<int>ans(size);
+void rotateArray(vector<int> arr, int n, int k)
+{
+    cout << "Calling function" << endl;
+    vector<int> ans(n);
 
-    for(int i=0;i<size;i++){
-        int j = (i + k) % size;
-        ans[j]= arr[i]; 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int j = (i + k) % n;
+    //     ans[j] = arr[i];
+    // }
+    // arr = ans;
+
+    for (int index = 0; index < n; index++)
+    {
+        int newIndex = (index + k) % n;
+        ans[newIndex] = arr[index];
     }
-    // arr=ans;
+    arr = ans;
 }
 
-int main(){
+int main()
+{
 
-    vector<int>arr ={10,20,30,40,50,60};
-    int size=arr.size();
-    int k=3;
-    rotateArray(arr,size,k);
-    
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+    vector<int> arr = {10, 20, 30, 40, 50, 60};
+    int n = arr.size();
+    int k = 3;
+    rotateArray(arr, n, k);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
