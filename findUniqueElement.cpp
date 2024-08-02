4554f010fdd3c6c7296eaf189e1df67fa297a1e0
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-02 17:18:05 +0530

    unique element in an array

--- findUniqueElement.cpp
+++ findUniqueElement.cpp
@@ -1,14 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void uniqueElement(int arr[],int size){
-    
+int uniqueElement(int arr[], int size)
+{
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
 }
 
-int main(){
+int main()
+{
 
-    int arr[9]={1,5,4,1,10,4,10,7,7,};
+    int arr[9] = {
+        1,
+        5,
+        4,
+        1,
+        10,
+        4,
+        10,
+        7,
+        7,
+    };
     int size = 9;
 
-    uniqueElement(arr,size);
+    int answer = uniqueElement(arr, size);
+    cout << "Th unique element is : " << answer << endl;
 }
