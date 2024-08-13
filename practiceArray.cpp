d188ca6c9ae9bd704a728fd250946ccd8e5702f0
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-13 16:39:13 +0530

    2d array print

--- practiceArray.cpp
+++ practiceArray.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void printArray(int arr[], int n)
+void printArray(int arr[3][3], int col, int row)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
-        cout << arr[i] << " ";
-    }
-}
-
-void takeArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main()
 {
-    int arr[6];
-    int n = 6;
-    takeArray(arr, n);
-    printArray(arr, n);
+    int arr[3][3] = {
+        {10, 20, 30},
+        {40, 50, 60},
+        {70, 80, 90},
+    };
+    int col = 3;
+    int row = 3;
+
+    printArray(arr, col, row);
 }
