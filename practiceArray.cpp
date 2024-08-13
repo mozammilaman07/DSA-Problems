f6c26c64fa235e09db70094a2067a1a11eb4bcd6
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-13 16:29:01 +0530

    Taking input array

--- practiceArray.cpp
+++ practiceArray.cpp
@@ -9,10 +9,18 @@ void printArray(int arr[], int n)
     }
 }
 
+void takeArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50, 60};
+    int arr[6];
     int n = 6;
-
+    takeArray(arr, n);
     printArray(arr, n);
 }
