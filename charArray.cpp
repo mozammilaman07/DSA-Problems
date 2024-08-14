6fc56445b7fa7ca29089f23f1ed4435d33517149
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-14 14:09:54 +0530

    taking input by using cin.getline()for adding space tab etc.

--- charArray.cpp
+++ charArray.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     char ch[15];
-    cin >> ch;
+    // cin >> ch;
+    cin.getline(ch, 15);
     cout << "Printing the char array : " << ch;
 
-    for (int i = 0; i < 15; i++)
-    {
-        cout << "at index : " << i << " " << ch[i] << endl;
-    }
+    //     for (int i = 0; i < 15; i++)
+    //     {
+    //         cout << "at index : " << i << " " << ch[i] << endl;
+    //     }
 }
