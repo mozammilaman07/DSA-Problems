6f748a5e671cece62abdd0ad7f629d262cdcaf91
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-14 13:59:57 +0530

    printing char array by  using for loop

--- charArray.cpp
+++ charArray.cpp
@@ -6,4 +6,9 @@ int main()
     char ch[15];
     cin >> ch;
     cout << "Printing the char array : " << ch;
+
+    for (int i = 0; i < 15; i++)
+    {
+        cout << "at index : " << i << " " << ch[i] << endl;
+    }
 }
