5a9b3b459925a56fbc223a3d30c83f4d179037dd
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-14 14:33:39 +0530

    created a funtion for string length

--- charArray.cpp
+++ charArray.cpp
@@ -2,12 +2,32 @@
 #include <string.h>
 using namespace std;
 
+int findLength(char ch[], int size)
+{
+    int length = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (ch[i] == '\0')
+        {
+            break;
+        }
+        else
+        {
+            length++;
+        }
+    }
+    return length;
+}
+
 int main()
 {
     char ch[20];
     // cin >> ch;
     cin.getline(ch, 20);
+    int length = findLength(ch, 20);
     cout << "Printing the char array : " << ch << endl;
+
+    cout << "size of string by functon : " << length << endl;
     cout << "size of string : " << strlen(ch) << endl;
 
     //     for (int i = 0; i < 15; i++)
