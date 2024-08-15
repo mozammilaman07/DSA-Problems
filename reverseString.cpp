e9db8b9df9a48cbb6477bec68c8f489834e9358d
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-15 22:44:41 +0530

    reverse String problem

--- reverseString.cpp
+++ reverseString.cpp
@@ -2,6 +2,18 @@
 #include <string.h>
 using namespace std;
 
+void reverseString(char ch[], int n)
+{
+    int i = 0;
+    int j = n - 1;
+    while (i <= j)
+    {
+        swap(ch[i], ch[j]);
+        i++;
+        j--;
+    }
+}
+
 int main()
 {
     char ch[20];
@@ -9,5 +21,8 @@ int main()
     cout << "Before reverse " << ch << endl;
 
     int length = strlen(ch);
-    cout << "Length " << length << endl;
+
+    reverseString(ch, length);
+
+    cout << "After " << ch << endl;
 }
