32efe8694a7e62e1e9c9aa63710a5c1396caade2
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-15 22:10:33 +0530

    re wrote the code

--- charArray.cpp
+++ charArray.cpp
@@ -2,38 +2,62 @@
 #include <string.h>
 using namespace std;
 
-int findLength(char ch[], int size)
-{
-
-    // Using for loop
-    // int length = 0;
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if (ch[i] == '\0')
-    //     {
-    //         break;
-    //     }
-    //     else
-    //     {
-    //         length++;
-    //     }
-    // }
-    // return length;
-}
-
 int main()
 {
+
     char ch[20];
-    // cin >> ch;
     cin.getline(ch, 20);
-    int length = findLength(ch, 20);
+
     cout << "Printing the char array : " << ch << endl;
-
-    cout << "size of string by functon : " << length << endl;
-    // cout << "size of string : " << strlen(ch) << endl;
-
-    //     for (int i = 0; i < 15; i++)
-    //     {
-    //         cout << "at index : " << i << " " << ch[i] << endl;
-    //     }
+    cout << "Printing the char array length : " << strlen(ch) << endl;
 }
+
+// #include <iostream>
+// #include <string.h>
+// using namespace std;
+
+// int findLength(char ch[], int size)
+// {
+//     // Using while loop
+
+//     int len = 0;
+//     int index = 0;
+//     while (ch[index] != '\0')
+//     {
+//         len++;
+//         index++
+//     }
+//     return len;
+
+//     // Using for loop
+//     // int length = 0;
+//     // for (int i = 0; i < size; i++)
+//     // {
+//     //     if (ch[i] == '\0')
+//     //     {
+//     //         break;
+//     //     }
+//     //     else
+//     //     {
+//     //         length++;
+//     //     }
+//     // }
+//     // return length;
+// }
+
+// int main()
+// {
+//     char ch[20];
+//     // cin >> ch;
+//     cin.getline(ch, 20);
+//     int length = findLength(ch, 20);
+//     cout << "Printing the char array : " << ch << endl;
+
+//     cout << "size of string by functon : " << length << endl;
+//     // cout << "size of string : " << strlen(ch) << endl;
+
+//     //     for (int i = 0; i < 15; i++)
+//     //     {
+//     //         cout << "at index : " << i << " " << ch[i] << endl;
+//     //     }
+// }
