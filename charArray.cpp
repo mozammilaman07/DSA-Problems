2621f59ffb96148d76e3833bfc5df5ad205b6ecd
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-08-15 21:59:12 +0530

    commented for loop

--- charArray.cpp
+++ charArray.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int findLength(char ch[], int size)
 {
-    int length = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (ch[i] == '\0')
-        {
-            break;
-        }
-        else
-        {
-            length++;
-        }
-    }
-    return length;
+
+    // Using for loop
+    // int length = 0;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if (ch[i] == '\0')
+    //     {
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         length++;
+    //     }
+    // }
+    // return length;
 }
 
 int main()
