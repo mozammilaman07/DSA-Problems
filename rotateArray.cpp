ef8b55695d8ec5a7cf3e34182294e1c50d4d88ea
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-15 18:38:56 +0530

    cleared prev logic func

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void rotateArray(vector<int> arr, int n, int k)
+void rotateArr(int arr[], int n, int k)
 {
-    cout << "Calling function" << endl;
-    vector<int> ans(n);
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int j = (i + k) % n;
-    //     ans[j] = arr[i];
-    // }
-    // arr = ans;
-
-    for (int index = 0; index < n; index++)
-    {
-        int newIndex = (index + k) % n;
-        ans[newIndex] = arr[index];
-    }
-    arr = ans;
 }
 
 int main()
 {
 
-    vector<int> arr = {10, 20, 30, 40, 50, 60};
-    int n = arr.size();
+    // vector<int> arr = {10, 20, 30, 40, 50, 60};
+    // int n = arr.size();
+    // int k = 3;
+    // rotateArray(arr, n, k);
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7;
     int k = 3;
-    rotateArray(arr, n, k);
+
+    rotateArr(arr, n, k);
 
     for (int i = 0; i < n; i++)
     {
